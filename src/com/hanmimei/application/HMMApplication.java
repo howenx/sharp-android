ec0c36d1b967fef084cda6a160a508b2b039809d
vince.liu <vince.liu@kakaocorp.com>
2016-02-03 18:03:41 +0800

    adsfkjhasdf

--- src/com/hanmimei/application/HMMApplication.java
+++ src/com/hanmimei/application/HMMApplication.java
@@ -12,14 +12,12 @@ import com.hanmimei.dao.DaoMaster.DevOpenHelper;
 import com.hanmimei.dao.DaoSession;
 import com.hanmimei.entity.User;
 import com.hanmimei.entity.VersionVo;
-import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.cache.memory.impl.UsingFreqLimitedMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
-import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import com.nostra13.universalimageloader.utils.StorageUtils;
 import com.testin.agent.TestinAgent;
 import com.testin.agent.TestinAgentConfig;
@@ -70,11 +68,7 @@ public class HMMApplication extends Application {
 				.threadPoolSize(3)// 线程池内加载的数量
 				.threadPriority(Thread.NORM_PRIORITY - 2)
 				.denyCacheImageMultipleSizesInMemory()
-				.memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024))// 你可以通过自己的内存缓存实现
-				.diskCache(new UnlimitedDiscCache(cacheDir))// 自定义缓存路径
-				.diskCacheSize(50 * 1024 * 1024)
 				.diskCacheFileNameGenerator(new Md5FileNameGenerator())// 将保存的时候的URI名称用MD5加密
-				.imageDownloader(new BaseImageDownloader(this, 5 * 1000, 30 * 1000)) // connectTimeout (5s),readTimeout(30s)超时时间
 				.defaultDisplayImageOptions(initImageLoaderDisplayImageOptions()).build();
 		return config;
 	}
@@ -85,6 +79,7 @@ public class HMMApplication extends Application {
 				.showImageForEmptyUri(R.drawable.ic_launcher)
 				.showImageOnFail(R.drawable.ic_launcher).cacheOnDisk(true)
 				.imageScaleType(ImageScaleType.EXACTLY).cacheInMemory(true)
+				.displayer(new FadeInBitmapDisplayer(500))//是否图片加载好后渐入的动画时间  
 				.build();
 		return imageOptions;
 	}
