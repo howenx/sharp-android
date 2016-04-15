3bdb124a9304a11d5a360f8ad62b4d828aa7fcb1
vince.liu <vince.liu@kakaocorp.com>
2016-04-15 14:15:34 +0800

    asdfkljjhkasdf

--- src/com/hanmimei/utils/upload/MultipartRequestParams.java
+++ src/com/hanmimei/utils/upload/MultipartRequestParams.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Logger;
 
 import org.apache.http.HttpEntity;
 
