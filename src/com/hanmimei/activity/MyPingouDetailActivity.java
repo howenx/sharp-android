0139c3252fdaabcf458dd417819f5a20e98741fb
vince.liu <vince.liu@kakaocorp.com>
2016-04-15 17:50:17 +0800

    asdfkha

--- src/com/hanmimei/activity/MyPingouDetailActivity.java
+++ src/com/hanmimei/activity/MyPingouDetailActivity.java
@@ -1,27 +1,32 @@
 package com.hanmimei.activity;
 
 import java.util.List;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hanmimei.R;
+import com.hanmimei.data.AppConstant;
 import com.hanmimei.data.DataParser;
 import com.hanmimei.data.UrlUtil;
 import com.hanmimei.entity.Order;
 import com.hanmimei.utils.ActionBarUtil;
+import com.hanmimei.utils.AlertDialogUtils;
+import com.hanmimei.utils.GlideLoaderUtils;
 import com.hanmimei.utils.Http2Utils;
 import com.hanmimei.utils.Http2Utils.VolleyJsonCallback;
-import com.hanmimei.utils.GlideLoaderUtils;
 import com.hanmimei.utils.ToastUtils;
 
 public class MyPingouDetailActivity extends BaseActivity {
 
 	private TextView order_state, order_price, order_addr, order_person,
 			pro_title, pro_guige;
-	private TextView order_no, order_time, btn_left, btn_right,pro_shipfee;
+	private TextView order_no, order_time, btn_left, btn_right, pro_shipfee;
 	private ImageView order_img, pro_img;
 
 	@Override
@@ -37,8 +42,12 @@ public class MyPingouDetailActivity extends BaseActivity {
 	private void loadPingouDetail() {
 		getLoading().show();
 		Log.i("orderId", getIntent().getStringExtra("orderId"));
-		Http2Utils.doGetRequestTask(this,getHeaders(),UrlUtil.GET_ORDER_LIST_URL + "/"
-						+ getIntent().getStringExtra("orderId"),new VolleyJsonCallback() {
+		Http2Utils.doGetRequestTask(
+				this,
+				getHeaders(),
+				UrlUtil.GET_ORDER_LIST_URL + "/"
+						+ getIntent().getStringExtra("orderId"),
+				new VolleyJsonCallback() {
 
 					@Override
 					public void onSuccess(String result) {
@@ -57,37 +66,58 @@ public class MyPingouDetailActivity extends BaseActivity {
 
 	private void initPingouDetail(List<Order> orders) {
 		if (orders != null && orders.size() > 0) {
-			Order o = orders.get(0);
+			final Order o = orders.get(0);
 			if (o.getOrderStatus().equals("S")) {
 				order_state.setText("等待配送");
 				order_img.setImageResource(R.drawable.hmm_order_submit);
 				btn_left.setVisibility(View.GONE);
 				btn_right.setVisibility(View.GONE);
-			}else if (o.getOrderStatus().equals("D")) {
+			} else if (o.getOrderStatus().equals("D")) {
 				order_state.setText("配送中");
 				order_img.setImageResource(R.drawable.hmm_distribution);
 				btn_left.setText("查看物流");
+				btn_left.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						Intent intent = new Intent(getActivity(),
+								LogisticsActivity.class);
+						intent.putExtra("orderId", o.getOrderId());
+						startActivity(intent);
+					}
+				});
 				btn_right.setText("确认收货");
-			}else if (o.getOrderStatus().equals("R")) {
+				btn_right.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						confirmDelivery(o);
+					}
+				});
+
+			} else if (o.getOrderStatus().equals("R")) {
 				order_state.setText("已签收");
 				order_img.setImageResource(R.drawable.hmm_sign);
 				btn_left.setText("退货");
 				btn_right.setText("评价");
-			}else{
+			} else {
 				order_state.setText("过期");
 				order_img.setImageResource(R.drawable.hmm_order_error);
 				btn_left.setVisibility(View.GONE);
 				btn_right.setVisibility(View.GONE);
 			}
-			order_addr.setText(o.getAdress().getCity()+o.getAdress().getAdress());
-			order_price.setText("¥"+o.getPayTotal());
+			order_addr.setText(o.getAdress().getCity()
+					+ o.getAdress().getAdress());
+			order_price.setText("¥" + o.getPayTotal());
 			order_no.setText(o.getOrderId());
-			order_person.setText(o.getAdress().getName() +"    "+o.getAdress().getPhone());
+			order_person.setText(o.getAdress().getName() + "    "
+					+ o.getAdress().getPhone());
 			order_time.setText(o.getOrderCreateAt());
-			pro_shipfee.setText("配送费："+o.getShipFee());
-			
-			GlideLoaderUtils.loadGoodsImage(getActivity(),o.getList().get(0).getInvImg(), pro_img);
-			pro_guige.setText("¥"+o.getList().get(0).getPrice());
+			pro_shipfee.setText("配送费：" + o.getShipFee());
+
+			GlideLoaderUtils.loadGoodsImage(getActivity(), o.getList().get(0)
+					.getInvImg(), pro_img);
+			pro_guige.setText("¥" + o.getList().get(0).getPrice());
 			pro_title.setText(o.getList().get(0).getSkuTitle());
 		} else {
 			ToastUtils.Toast(this, R.string.error);
@@ -108,6 +138,37 @@ public class MyPingouDetailActivity extends BaseActivity {
 		pro_shipfee = (TextView) findViewById(R.id.pro_shipfee);
 		order_img = (ImageView) findViewById(R.id.order_img);
 		pro_img = (ImageView) findViewById(R.id.pro_img);
+	}
+
+	// 确认收货操作
+	private void confirmDelivery(final Order order) {
+		AlertDialogUtils.showDeliveryDialog(this, new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				confirmDeliveryR(order);
+			}
+		});
+	}
+
+	private void confirmDeliveryR(Order order) {
+		getLoading().show();
+		Http2Utils.doGetRequestTask(this, getHeaders(),
+				UrlUtil.CONFIRM_DELIVERY + order.getOrderId(),
+				new VolleyJsonCallback() {
+
+					@Override
+					public void onSuccess(String result) {
+						getLoading().dismiss();
+						sendBroadcast(new Intent(
+								AppConstant.MESSAGE_BROADCAST_CANCLE_ORDER));
+					}
+
+					@Override
+					public void onError() {
+						getLoading().dismiss();
+					}
+				});
 	}
 
 }
