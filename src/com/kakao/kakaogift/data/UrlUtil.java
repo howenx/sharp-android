7e5d07e1738ec3283e30ac9ba732ec4efa9a7d1a
eric <eric.song@kakaocorp.com>
2016-08-05 15:20:28 +0800

    adsf

--- src/com/kakao/kakaogift/data/UrlUtil.java
+++ src/com/kakao/kakaogift/data/UrlUtil.java
@@ -8,6 +8,14 @@ public class UrlUtil {
 //	 public static final String SERVERY1 = "https://shopping.kakaogift.cn";
 //	 public static final String SERVERY4 = "https://id.kakaogift.cn";
 //	 public static final String SERVERY5 = "https://promotion.kakaogift.cn";
+	 /*
+	  * 阿里云
+	  */
+	 public static final String SERVERY3 = "http://api.hanmimei.com";
+	 public static final String SERVERY1 = "http://shopping.hanmimei.com";
+	 public static final String SERVERY2 = "http://cash.hanmimei.com";
+	 public static final String SERVERY4 = "http://id.hanmimei.com";
+	 public static final String SERVERY5 = "http://promotion.hanmimei.com";
 	/*
 	 * http://172.28.3.78	孙宝娜
 	 */
@@ -25,10 +33,10 @@ public class UrlUtil {
 	/*
 	 * http://172.28.3.18	内部测试
 	 */
-	 public static final String SERVERY3 = "http://172.28.3.18:9001";
-	 public static final String SERVERY1 = "http://172.28.3.18:9003";
-	 public static final String SERVERY4 = "http://172.28.3.18:9004";
-	 public static final String SERVERY5 = "http://172.28.3.18:9005";
+//	 public static final String SERVERY3 = "http://172.28.3.18:9001";
+//	 public static final String SERVERY1 = "http://172.28.3.18:9003";
+//	 public static final String SERVERY4 = "http://172.28.3.18:9004";
+//	 public static final String SERVERY5 = "http://172.28.3.18:9005";
 
 	/*
 	 * http://172.28.3.46	武多文
@@ -116,7 +124,7 @@ public class UrlUtil {
 	public static final String POST_CLIENT_ORDER_SUBMIT = SERVERY1
 			+ "/client/order/submit";
 	//
-	public static final String CLIENT_PAY_ORDER_GET = SERVERY1
+	public static final String CLIENT_PAY_ORDER_GET = SERVERY2
 			+ "/client/pay/order/get/";
 	// 退货申请接口
 	public static final String CUSTOMER_SERVICE_APPLY = SERVERY1
